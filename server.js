0c78b75e9a868d7fa97145486416d3efa52671d8
tpreston96 <tpreston96@gmail.com>
2020-11-30 19:47:23 -0600

    fixed nav partial

--- server.js
+++ server.js
@@ -11,5 +11,18 @@ app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
 
+// Mount routes
+app.get('/', function(req, res) {
+    res.send('<h1>Hello Express</h1>');
+});
+
+app.get('/home', function(req, res) {
+    res.render('home');
+});
 
 
+
+// Tell the app to listen on port 3000
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+  });
