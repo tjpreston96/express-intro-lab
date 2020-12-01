5d26fee5350f053ddc83d10fa4b6843a5fb0a653
tpreston96 <tpreston96@gmail.com>
2020-11-30 19:37:41 -0600

    added ejs and nav partial

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
-//=========================================================
-//                  express app
-//=========================================================
+// create express app
 
 const app = express();
+
+// configure the app(app.set)
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+
+
