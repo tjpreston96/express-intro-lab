98c30721477c125e2ab254d6cf908b2f5e70103e
tpreston96 <tpreston96@gmail.com>
2020-11-30 20:20:47 -0600

    changed topic and reframed

--- server.js
+++ server.js
@@ -1,28 +1,50 @@
 const express = require('express');
 const path = require('path');
 
-// create express app
-
+//=========================================================
+//                create express app
+//=========================================================
 const app = express();
 
-// configure the app(app.set)
-
+//=========================================================
+//             configure the app(app.set)
+//=========================================================
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
 
-// Mount routes
-app.get('/', function(req, res) {
-    res.send('<h1>Hello Express</h1>');
-});
-
-app.get('/home', function(req, res) {
-    res.render('home');
-});
-
-
-
-// Tell the app to listen on port 3000
+//=========================================================
+//          Tell the app to listen on port 3000
+//=========================================================
 app.listen(3000, function() {
     console.log('Listening on port 3000');
-  });
+});
+
+//=========================================================
+//      Mount routes ---- middleware & static files
+//=========================================================
+app.get('/', function(req, res) {
+    res.render('home', { title: 'Home'});
+});
+
+app.get('/github', function(req, res) {
+    res.render('github', { title: 'Github'});
+});
+
+app.get('/projects', function(req, res) {
+    res.render('projects', { title: 'Projects'});
+});
+
+//=========================================================
+//                   Redirects
+//=========================================================
+app.get('/home', (req, res) => {
+    res.redirect('/');
+});
+
+//=========================================================
+//                    404 Page
+//=========================================================
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' })
+});
